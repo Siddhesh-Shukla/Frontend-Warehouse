632487c03e95a319b381f80dcadb2666f2137142
Siddhesh-Shukla <siddheshshukla1024@gmail.com>
2022-01-02 13:38:37 +0530

    EditPage Added

--- main/views.py
+++ main/views.py
@@ -16,3 +16,19 @@ def index(request):
                 'msg': ""
             }
             return JsonResponse(response)
+
+def edit(request):
+    if request.method =='GET':
+        return render(request, 'main/editPage.html')
+    elif request.is_ajax():
+        data_entered = request.POST.get('text', None)
+        if data_entered:
+            response = {
+                'msg': data_entered
+            }
+            return JsonResponse(response)
+        else:
+            response = {
+                'msg': ""
+            }
+            return JsonResponse(response)
