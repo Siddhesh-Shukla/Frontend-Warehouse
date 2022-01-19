3fea345ad7c623920f8d2b18ac5a3c506eba873d
Siddhesh-Shukla <siddheshshukla1024@gmail.com>
2022-01-19 19:51:52 +0530

    PostListView Added

--- main/views.py
+++ main/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Post
+
 
 def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
     if request.method =='GET':
-        return render(request, 'main/index.html')
+        return render(request, 'main/index.html', context)
     elif request.is_ajax():
         data_entered = request.POST.get('text', None)
         if data_entered:
@@ -18,17 +23,17 @@ def home(request):
             return JsonResponse(response)
 
 def workspace(request):
-    if request.method =='GET':
-        return render(request, 'main/editPage.html')
-    elif request.is_ajax():
-        data_entered = request.POST.get('text', None)
-        if data_entered:
-            response = {
-                'msg': data_entered
-            }
-            return JsonResponse(response)
-        else:
-            response = {
-                'msg': ""
-            }
-            return JsonResponse(response)
+    # if request.method =='GET':
+    return render(request, 'main/editPage.html')
+    # elif request.is_ajax():
+    #     data_entered = request.POST.get('text', None)
+    #     if data_entered:
+    #         response = {
+    #             'msg': data_entered
+    #         }
+    #         return JsonResponse(response)
+    #     else:
+    #         response = {
+    #             'msg': ""
+    #         }
+    #         return JsonResponse(response)
