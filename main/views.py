273dfbd9aea010be4a3be1d9e5bc687664dc2b8d
Siddhesh-Shukla <siddheshshukla1024@gmail.com>
2022-01-20 09:43:44 +0530

    Bugs of Post Form Fixed + CSS Added

--- main/views.py
+++ main/views.py
@@ -12,8 +12,11 @@ def home(request):
     context = {
         'posts': Post.objects.all()
     }
+    return render(request, 'main/index.html', context)
+
+def workspace(request):
     if request.method =='GET':
-        return render(request, 'main/index.html', context)
+        return render(request, 'main/editPage.html')
     elif request.is_ajax():
         data_entered = request.POST.get('text', None)
         if data_entered:
@@ -27,28 +30,12 @@ def home(request):
             }
             return JsonResponse(response)
 
-def workspace(request):
-    # if request.method =='GET':
-    return render(request, 'main/editPage.html')
-    # elif request.is_ajax():
-    #     data_entered = request.POST.get('text', None)
-    #     if data_entered:
-    #         response = {
-    #             'msg': data_entered
-    #         }
-    #         return JsonResponse(response)
-    #     else:
-    #         response = {
-    #             'msg': ""
-    #         }
-    #         return JsonResponse(response)
-
 class UserPostListView(ListView):
     model = Post
     template_name = 'main/user_post.html'
     context_object_name = 'posts'
     # ordering = ['-date_posted']
-    paginate_by = 2
+    # paginate_by = 2
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
